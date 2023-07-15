d2f9cc8d6a4b5ad523cee35c5edee06d9e598238
OmarRamzy45 <ramzyomar0@gmail.com>
2023-07-15 20:06:55 +0300

    update testing

--- test_function_plotter.py
+++ test_function_plotter.py
@@ -1,24 +1,76 @@
 import sys
 import pytest
 from PySide2.QtWidgets import QApplication
-from function_plotter import MainWindow
+from PySide2.QtTest import QTest
+from PySide2.QtCore import Qt
+from main import MainWindow
 
 @pytest.fixture
 def app(qtbot):
-    application = QApplication(sys.argv) # Create the application
-    yield application # Tests will run here
-    application.quit() # Exit the application
+    application = QApplication(sys.argv)
+    yield application
+    application.quit()
 
-def test_invalid_function(app, qtbot):
-    window = MainWindow() # Create the main window
-    qtbot.addWidget(window) # Add the widget to the qtbot for testing
 
-    # Set up invalid function input
-    qtbot.keyClicks(window.input_layouts[0][1], "x^")
+def test_valid_input_plot(app, qtbot):
+    window = MainWindow()
+    qtbot.addWidget(window)
+
+    # Set up valid function input
+    qtbot.keyClicks(window.input_layouts[0][1], "x^2")
+
+    # Set up valid min and max values
+    qtbot.keyClicks(window.input_layouts[1][1], "-5")
+    qtbot.keyClicks(window.input_layouts[2][1], "5")
 
     # Click the plot button
     qtbot.mouseClick(window.plot_button, Qt.LeftButton)
 
-    # Assert that an error message is displayed
-    assert window.error_label.text() == "Error: Invalid syntax in the function."
+    # Assert that no error message is displayed
+    assert not window.error_label.text()
 
+    # Assert that the plot appears on the canvas
+    assert window.canvas.figure.get_axes()
+
+    # Check if the plot is a line plot
+    assert all(isinstance(item, plt.Line2D) for item in window.canvas.figure.get_axes()[0].get_children())
+
+
+def test_grid_checkbox(app, qtbot):
+    window = MainWindow()
+    qtbot.addWidget(window)
+
+    # Check if the grid is initially off
+    assert not window.grid_checkbox.isChecked()
+
+    # Click the grid checkbox
+    qtbot.mouseClick(window.grid_checkbox, Qt.LeftButton)
+
+    # Check if the grid is turned on
+    assert window.grid_checkbox.isChecked()
+
+    # Click the grid checkbox again
+    qtbot.mouseClick(window.grid_checkbox, Qt.LeftButton)
+
+    # Check if the grid is turned off again
+    assert not window.grid_checkbox.isChecked()
+
+
+def test_axes_checkbox(app, qtbot):
+    window = MainWindow()
+    qtbot.addWidget(window)
+
+    # Check if the axes are initially off
+    assert not window.axes_checkbox.isChecked()
+
+    # Click the axes checkbox
+    qtbot.mouseClick(window.axes_checkbox, Qt.LeftButton)
+
+    # Check if the axes are turned on
+    assert window.axes_checkbox.isChecked()
+
+    # Click the axes checkbox again
+    qtbot.mouseClick(window.axes_checkbox, Qt.LeftButton)
+
+    # Check if the axes are turned off again
+    assert not window.axes_checkbox.isChecked()
