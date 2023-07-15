de5568c0499a03f7259f42957694112f09a00137
OmarRamzy45 <ramzyomar0@gmail.com>
2023-07-15 20:02:27 +0300

    plotter testing file

--- test_function_plotter.py
+++ test_function_plotter.py
@@ -1,46 +1,24 @@
 import sys
 import pytest
 from PySide2.QtWidgets import QApplication
-from main import MainWindow
+from function_plotter import MainWindow
 
 @pytest.fixture
 def app(qtbot):
-    application = QApplication(sys.argv)
-    yield application
-    application.quit()
+    application = QApplication(sys.argv) # Create the application
+    yield application # Tests will run here
+    application.quit() # Exit the application
 
-def test_plot_button(app, qtbot):
-    window = MainWindow()
-    qtbot.addWidget(window)
+def test_invalid_function(app, qtbot):
+    window = MainWindow() # Create the main window
+    qtbot.addWidget(window) # Add the widget to the qtbot for testing
 
-    # Set up initial values
-    qtbot.keyClicks(window.input_layouts[0][1], "x**2")
-    qtbot.keyClicks(window.input_layouts[1][1], "0")
-    qtbot.keyClicks(window.input_layouts[2][1], "10")
+    # Set up invalid function input
+    qtbot.keyClicks(window.input_layouts[0][1], "x^")
 
     # Click the plot button
     qtbot.mouseClick(window.plot_button, Qt.LeftButton)
 
-    # Assert that the plot is displayed
-    assert window.figure.axes
+    # Assert that an error message is displayed
+    assert window.error_label.text() == "Error: Invalid syntax in the function."
 
-def test_restart_button(app, qtbot):
-    window = MainWindow()
-    qtbot.addWidget(window)
-
-    # Set up initial values
-    qtbot.keyClicks(window.input_layouts[0][1], "x**2")
-    qtbot.keyClicks(window.input_layouts[1][1], "0")
-    qtbot.keyClicks(window.input_layouts[2][1], "10")
-
-    # Click the plot button
-    qtbot.mouseClick(window.plot_button, Qt.LeftButton)
-
-    # Click the restart button
-    qtbot.mouseClick(window.restart_button, Qt.LeftButton)
-
-    # Assert that the input fields are cleared and the plot is cleared
-    assert window.input_layouts[0][1].text() == ""
-    assert window.input_layouts[1][1].text() == ""
-    assert window.input_layouts[2][1].text() == ""
-    assert not window.figure.axes
